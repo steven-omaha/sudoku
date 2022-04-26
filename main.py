8a2e270817b513c9ace7ee417c18994041a5ada6
timeshifter <timeshifter@sumatrae>
2022-04-26 22:01:09 +0200

    start refactor eliminate

--- main.py
+++ main.py
@@ -65,29 +65,33 @@ def assign(values, s, d):
         return False
 
 
-def eliminate(values, s, d):
+def eliminate(values: dict[str, str], s, d):
     """Eliminate d from values[s]; propagate when values or places <= 2.
     Return values, except return False if a contradiction is detected."""
-    if d not in values[s]:
-        return values  ## Already eliminated
-    values[s] = values[s].replace(d, '')
-    ## (1) If a square s is reduced to one value d2, then eliminate d2 from the peers.
-    if len(values[s]) == 0:
-        return False  ## Contradiction: removed last value
-    elif len(values[s]) == 1:
-        d2 = values[s]
-        if not all(eliminate(values, s2, d2) for s2 in peers[s]):
-            return False
-    ## (2) If a unit u is reduced to only one place for a value d, then put it there.
+    if d in values[s]:
+        values[s] = values[s].replace(d, '')
+        ## (1) If a square s is reduced to one value d2, then eliminate d2 from the peers.
+        if len(values[s]) == 1:
+            f2(s, values)
+        ## (2) If a unit u is reduced to only one place for a value d, then put it there.
+        else:
+            f3(d, s, values)
+    return values
+
+
+def f2(s, values):
+    d2 = values[s]
+    for s2 in peers[s]:
+        eliminate(values, s2, d2)
+
+
+def f3(d, s, values):
     for u in units[s]:
         dplaces = [s for s in u if d in values[s]]
-        if len(dplaces) == 0:
-            return False  ## Contradiction: no place for this value
-        elif len(dplaces) == 1:
+        if len(dplaces) == 1:
             # d can only be in one place in unit; assign it there
             if not assign(values, dplaces[0], d):
-                return False
-    return values
+                break
 
 
 def display(values):
